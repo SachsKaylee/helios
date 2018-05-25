87f4464dcb65dcf614dbfa76401602b70e41379f
Patrick Sachs <kontakt@patrick-sachs.de>
2018-05-25 17:49:44 +0200

    Bulma is now built from source instead of loaded from a CDN

---
+++ next.config.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const webpack = require('webpack');
+const path = require('path');
+const glob = require('glob');
+
+module.exports = {
+  webpack: (config, { dev }) => {
+    // Setup SASS
+    config.module.rules.push(
+      {
+        test: /\.(css|scss)/,
+        loader: 'emit-file-loader',
+        options: {
+          name: 'dist/[path][name].[ext]'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['babel-loader', 'raw-loader', 'postcss-loader']
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          'babel-loader',
+          'raw-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['styles', 'node_modules']
+                .map(d => path.join(__dirname, d))
+                .map(g => glob.sync(g))
+                .reduce((a, c) => a.concat(c), [])
+            }
+          }
+        ]
+      }
+    );
+
+    // Setup environment variables
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+
+    return config;
+  }
+};
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,45 +1,42 @@
 import Head from "next/head";
 import Navbar from "./Navbar";
+import style from "../../styles/style.scss";
+import { Container } from 'bloomer';
 
 export default (({ title, children }) => (
-  <div>
+  <html>
     <Head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       <title key="title">{title} | Patrick Sachs</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css" />
+      <style dangerouslySetInnerHTML={{ __html: style }} />
       <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
     </Head>
-    <Navbar
-      title={"Patrick Sachs - " + title}
-      logo="/static/content/system/logo.png"
-      links={[{
-        title: "Home",
-        link: "/"
-      }, {
-        title: "Projects",
-        link: "/projects"
-      }, {
-        title: "Admin",
-        link: "/admin",
-        children: [{
-          title: "New Post",
-          link: "/admin/post"
+    <body>
+      <Navbar
+        title={"Patrick Sachs - " + title}
+        logo="/static/content/system/logo.png"
+        links={[{
+          title: "Home",
+          link: "/"
         }, {
-          title: "Account",
-          link: "/admin/account"
-        }]
-      }]} />
-    <div className="container">
-      {children}
-    </div>
-    <style jsx>
-      {`html, body {
-        font-family: 'Open Sans', sans-serif;
-        font-size: 14px;
-        background: #F0F2F4;
-      }`}
-    </style>
-  </div>
+          title: "Projects",
+          link: "/projects"
+        }, {
+          title: "Admin",
+          link: "/admin",
+          children: [{
+            title: "New Post",
+            link: "/admin/post"
+          }, {
+            title: "Account",
+            link: "/admin/account"
+          }]
+        }]} />
+      <Container>
+        {children}
+      </Container>
+    </body>
+  </html>
 ));
