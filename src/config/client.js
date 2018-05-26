ae841f9c19f0642b5316d0784d7dc5e82b38eaf5
Patrick Sachs <kontakt@patrick-sachs.de>
2018-05-26 21:39:46 +0200

    Added basic manifest

--- src/config/client.js
+++ src/config/client.js
@@ -1,4 +1,5 @@
 module.exports = {
   title: "Helios",
+  description: "A minimalistic CMS using Node.js and React.",
   maxAvatarSize: 200 * 1024
 }
---
+++ src/server/robots.js
@@ -0,0 +1,30 @@
+const config = require("../config/server");
+
+const install = ({ server, $send }) => {
+  server.get("/manifest.json", (req, res) => {
+    $send(res, {
+      data: {
+        "short_name": config.client.title,
+        "name": config.client.title,
+        "description": config.client.description,
+        "lang": "en-US", // todo: Localize Helios one day
+        "icons": [
+          makeManifestIcon(256)
+        ],
+        "start_url": "/",
+        "scope": "/",
+        "theme_color": "aliceblue",
+        "background_color": "white",
+        "display": "standalone"
+      }
+    })
+  });
+}
+
+const makeManifestIcon = size => ({
+  "src": `/static/content/system/logo-${size}x${size}.png`,
+  "type": "image/png",
+  "sizes": `${size}x${size}`
+});
+
+module.exports = { install }
