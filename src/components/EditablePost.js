4d4de43e5b6e0833e971880c52d66c84c484f677
Patrick Sachs <kontakt@patrick-sachs.de>
2018-06-19 18:27:52 +0200

    Adjusted cursor in title of the editable post to look like an input

--- src/components/EditablePost.js
+++ src/components/EditablePost.js
@@ -7,18 +7,18 @@ const createEditor = ({ value, name, onChange }) => (<Editor
   value={value}
   onChange={onChange && onChange(name)} />);
 
-const EditablePost = ({  author, date, title, content,  onChange}) => (
-    <Card
-      image={`/api/avatar/${author}`}
-      title={<A href={undefined}>{createEditor({ onChange, value: title, name: "title" })}</A>}
-      subtitle={<FormattedMessage id="post.subtitle" values={{
-        author: <A href={undefined}>{author}</A>,
-        date
-      }} />}>
-      <div>
-        {createEditor({ onChange, value: content, name: "content" })}
-      </div>
-    </Card>
-  );
+const EditablePost = ({ author, date, title, content, onChange }) => (
+  <Card
+    image={`/api/avatar/${author}`}
+    title={<A href={undefined} style={{ cursor: "text" }}>{createEditor({ onChange, value: title, name: "title" })}</A>}
+    subtitle={<FormattedMessage id="post.subtitle" values={{
+      author: <A href={undefined}>{author}</A>,
+      date
+    }} />}>
+    <div>
+      {createEditor({ onChange, value: content, name: "content" })}
+    </div>
+  </Card>
+);
 
 export default EditablePost;
