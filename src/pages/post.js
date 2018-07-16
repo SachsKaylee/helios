d8b9101a5c6804f1329e64245cdc308397f02a8b
Patrick Sachs <kontakt@patrick-sachs.de>
2018-07-16 21:57:46 +0200

    Hide post edit button for non authorized users

--- src/pages/post.js
+++ src/pages/post.js
@@ -3,6 +3,7 @@ import React from "react"
 import axios from "axios"
 import Head from "next/head";
 import config from "../config/client";
+import Store from "../store";
 
 export default class extends React.Component {
   static async getInitialProps(p) {
@@ -22,13 +23,15 @@ export default class extends React.Component {
         <meta key="author" name="author" content={post.author} />
         <meta key="description" name="description" content={post.title} />
       </Head>
-      <Post
-        edit={["show-admin-buttons"]}
-        id={post._id}
-        date={new Date(post.date)}
-        author={post.author}
-        title={post.title}
-        content={post.content} />
+      <Store.Consumer>
+        {store => (<Post
+          edit={[store && store.hasPermission("author") && "show-admin-buttons"]}
+          id={post._id}
+          date={new Date(post.date)}
+          author={post.author}
+          title={post.title}
+          content={post.content} />)}
+      </Store.Consumer>
     </>);
   }
 };
