cdbbf0ef3015a72d783a83adb2e4f8ad3070936b
PatrickSachs <kontakt@patrick-sachs.de>
2018-11-12 23:04:05 +0100

    Update fields to use Jodit

--- src/components/EditorRichText.js
+++ src/components/EditorRichText.js
@@ -1,38 +1,15 @@
-import React from "react";
-import { Editor } from 'slate-react';
-import { serializeSingle } from "../simple-html-serializer";
-import { Value } from "slate";
-import Plain from "slate-plain-serializer";
-
-export const dataToValue = (value = "") => {
-  return "string" === (typeof value)
-    ? Plain.deserialize(value)
-    : Value.isValue(value)
-      ? value
-      : Value.fromJSON(value);
-}
+import * as React from "react";
+import 'jodit/build/jodit.min.css';
+import Dynamic from "../components/Dynamic";
 
 export default class EditorRichText extends React.Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+
   render() {
-    const { rules, ...props } = this.props;
-    return (<div>
-      <Editor
-        renderNode={this.renderNode}
-        renderMark={this.renderMark}
-        ref={this.props.editorRef}
-        {...props} />
-    </div>);
-  }
-
-  renderNode = props => {
-    const { rules } = this.props;
-    const { attributes, children, node } = props;
-    return serializeSingle(rules, node, children, attributes);
-  }
-
-  renderMark = props => {
-    const { rules } = this.props;
-    const { attributes, children, mark } = props;
-    return serializeSingle(rules, mark, children, attributes);
+    return (<Dynamic {...this.props} dynamic={{
+      loader: () => import("jodit").then(() => import("jodit-react"))
+    }}/> );
   }
 }
