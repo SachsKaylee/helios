72f3b5ef3be8c491633777ed17634b2066cc9b41
PatrickSachs <kontakt@patrick-sachs.de>
2018-11-24 01:26:00 +0100

    Notifications can have a timeout

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -4,23 +4,25 @@ import classnames from "classnames";
 export default class Notification extends React.PureComponent {
   constructor(p) {
     super(p);
+    this.onClose = this.onClose.bind(this);
     this.state = {
       closed: false
     }
   }
 
-  onClose = () => {
+  onClose() {
     const { onClose } = this.props;
     this.setState({ closed: true }, () => onClose && onClose());
   }
 
   render() {
-    const { canClose, children, title, icon: Icon, type } = this.props;
+    const { canClose, children, timeout, elapsed, title, icon: Icon, type } = this.props;
     const { closed } = this.state;
     return (!closed && (<div className={classnames("pop-in", "notification", type && "is-" + type)}>
       {canClose && (<button className="delete" onClick={this.onClose}></button>)}
-      {(title || Icon) && <p><Icon />{title}</p>}
+      {(title || Icon) && <p><Icon className="mdi-icon-spacer" /><strong>{title}</strong></p>}
       {children}
+      {timeout && (<progress className="progress is-small overlay-notification-progress" value={elapsed} max={timeout} />)}
     </div>));
   }
 }
--- src/store/Notification.js
+++ src/store/Notification.js
@@ -3,30 +3,71 @@ import { uuid } from "../utils/uuid";
 
 const Context = React.createContext();
 
+const TICK_INTERVAL = 15;
+
 export class NotificationProvider extends React.PureComponent {
   constructor(p) {
     super(p);
     this.notifications = React.createRef();
     this.push = this.push.bind(this);
     this.close = this.close.bind(this);
+    this.interval = this.interval.bind(this);
+    this.maybeStartTimer = this.maybeStartTimer.bind(this);
+    this.maybeEndTimer = this.maybeEndTimer.bind(this);
+    this.timeout = null;
     this.state = {
       notifications: []
     };
   }
 
-  push({ _id = uuid(), title, icon, type, canClose, children }) {
+  componentWillUnmount() {
+    this.maybeEndTimer(true);
+  }
+
+  maybeStartTimer() {
+    if (!this.timeout) {
+      this.timeout = setInterval(this.interval, TICK_INTERVAL);
+    }
+  }
+
+  maybeEndTimer(force = false) {
+    if (!this.timeout) {
+      return;
+    }
+    if (!force && this.state.notifications.length !== 0) {
+      return;
+    }
+    if (force) {
+      console.warn("Forcing Notification timer to stop. Some notifications may get stuck:", this.state.notifications);
+    }
+    this.timeout = clearInterval(this.timeout);
+  }
+
+  /**
+   * Called at regular intervals to update the notifications.
+   */
+  interval() {
+    console.log("tick....")
+    this.setState(s => ({
+      notifications: s.notifications
+        .map(n => ({ ...n, elapsed: n.elapsed + TICK_INTERVAL }))
+        .filter(n => !n.timeout || n.elapsed <= n.timeout)
+    }), this.maybeEndTimer);
+  }
+
+  push({ _id, title, icon, type, timeout, canClose, children }) {
     this.setState(s => ({
       notifications: [
         ...s.notifications,
-        { _id, type, children, canClose }
+        { _id: _id || uuid(), elapsed: 0, title, icon, type, timeout, children, canClose }
       ]
-    }));
+    }), this.maybeStartTimer);
   }
 
   close(id) {
     this.setState(s => ({
       notifications: s.notifications.filter(n => n._id != id)
-    }));
+    }), this.maybeEndTimer);
   }
 
   render() {
