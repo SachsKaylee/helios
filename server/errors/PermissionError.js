4366fa7dcc4df35361b269f3d1c620fd68537da7
PatrickSachs <kontakt@patrick-sachs.de>
2019-04-20 22:39:58 +0200

    Every user has their own user directory

--- server/errors/PermissionError.js
+++ server/errors/PermissionError.js
@@ -1,6 +1,10 @@
 const HeliosError = require("./HeliosError");
 
 module.exports = class PermissionError extends HeliosError {
+  /**
+   * Creates the error. This error is used to indicate that the user lacks a given permission to perform a certain action.
+   * @param {string} permission The missing permission.
+   */
   constructor(permission) {
     super(permission ? "Missing permission: " + permission : "Missing permission", 403);
     this.details = {
